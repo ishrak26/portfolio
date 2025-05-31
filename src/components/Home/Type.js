05b7f0ff568b9e9c9138f27228e3c5ec57b45c6a
Md. Ishrak Ahsan <ishrak26@gmail.com>
2025-05-31 15:08:32 +0600

    Initial commit
    Modified initial home and others

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -1,22 +1,22 @@
-import React from "react";
-import Typewriter from "typewriter-effect";
+import React from 'react';
+import Typewriter from 'typewriter-effect';
 
 function Type() {
-  return (
-    <Typewriter
-      options={{
-        strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-      }}
-    />
-  );
+    return (
+        <Typewriter
+            options={{
+                strings: [
+                    'Teacher',
+                    'Researcher',
+                    'Former Competitive Programmer',
+                    'Full Stack Developer',
+                ],
+                autoStart: true,
+                loop: true,
+                deleteSpeed: 50,
+            }}
+        />
+    );
 }
 
 export default Type;
